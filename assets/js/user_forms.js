e94794ee7ed67e6086aa12f5c7ecb82ea77d62fb
3tuchka3 <enotostated2.0@mail.ru>
2022-03-27 17:04:25 +0300

    1231qqaqqxvbf

--- assets/js/user_forms.js
+++ assets/js/user_forms.js
@@ -5,7 +5,7 @@
 const lowerCaseLatters = /[a-z]/g;
 const upperCaseLatters = /[A-Z]/g;
 const numbers = /[0-9]/g;
-// const validate_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const validate_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
  const database = new Database();
 
 let user_login = {
@@ -13,22 +13,22 @@ let user_login = {
     "password": '',
     "validate_data": function(){
         if(this.login == '' || this.password == '' || this.email == '' && this.email != null){
-            return 'поле не заполнено';
+            return 'The field is not filled';
         }
-        // else if(validate_email.test(this.email) == false && this.email != null){
-        //     return 'Incorrect email address';
-        // }
+        else if(validate_email.test(this.email) == false && this.email != null){
+           return 'Incorrect email address';
+         }
         else if(database.onLoginIsBusy(this.login) == true && this.email != null){
             return 'Этот логин занят';
         }
         else if(lowerCaseLatters.test(this.password) == false){
-            return 'В пароле отсутствует нижний регистр';
+            return 'Password is missing lowercase';
         }
         else if(upperCaseLatters.test(this.password) == false){
-            return 'В пароле отсутствуют заглавные буквы';
+            return 'Password missing uppercase';
         }
         else if(numbers.test(this.password) == false){
-            return 'В пароле отсутствуют цифры';
+            return 'Password missing numbers';
         }
         return null;
     }
